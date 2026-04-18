64375a5cd73e0454975610cc9b25eb80c10c5b79
sakoichi <kouichi19980817@gmail.com>
2026-04-18 17:38:54 +0900

    first commit

---
+++ src/main/java/com/froggyy351/bookblog/BookblogApplication.java
@@ -0,0 +1,13 @@
+package com.froggyy351.bookblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/froggyy351/bookblog/BookblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.froggyy351.bookblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
