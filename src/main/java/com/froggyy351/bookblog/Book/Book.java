a4acdc0ef529b326d4739caf63df4a2d5dc13cfc
sakoichi <kouichi19980817@gmail.com>
2026-04-24 00:01:58 +0900

    feat: Entityクラスを作成(Book.java)

---
+++ src/main/java/com/froggyy351/bookblog/Book/Book.java
@@ -0,0 +1,111 @@
+package com.froggyy351.bookblog.Book;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "book")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //自動生成 Postgres側でCreateTableする時「serial」にする必要あり
+    private int id;
+
+    @Column(name = "title" ,nullable = false)
+    private String title;
+
+    @Column(name = "author" ,nullable = false)
+    private String author;
+
+    @Column(name = "isbn" ,nullable = false)
+    private String isbn;
+
+    @Column(name = "title")
+    private String image;
+
+    @Column(name = "memo")
+    private String memo;
+
+    @Column(name = "status" ,nullable = false)
+    private int status;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public void setMemo(String memo) {
+        this.memo = memo;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
