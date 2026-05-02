680eb568b5a3f45bf9bd01a7c5cbc90c2822deb5
sakoichi <kouichi19980817@gmail.com>
2026-05-02 22:48:59 +0900

    feat: APIでのCRUD処理実装完了（Postmanでのテストも完了）

--- src/main/java/com/froggyy351/bookblog/Book/BookController.java
+++ src/main/java/com/froggyy351/bookblog/Book/BookController.java
@@ -1,13 +1,13 @@
 package com.froggyy351.bookblog.Book;
 
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/books")
 public class BookController {
 
     private final BookRepository bookRepository;
@@ -19,9 +19,41 @@ public class BookController {
         this.bookRepository = bookRepository;
     }
 
-    @PostMapping("/create")
+    @PostMapping
     public Book createBook(@RequestBody Book book){
         // save()は、保存したオブジェクトが戻り値になる
         return bookRepository.save(book);
+    }
+
+    @GetMapping
+    public List<Book> selectBooks(){
+        return bookRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Book selectBooks(@PathVariable long id){
+        return bookRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"該当のIDは存在していません。"));
+    }
+
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable long id, @RequestBody Book book){
+        Book exsistingBook = bookRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"該当のIDは存在していません。"));
+        exsistingBook.setAuthor(book.getAuthor());
+        exsistingBook.setTitle(book.getTitle());
+        exsistingBook.setImage(book.getImage());
+        exsistingBook.setIsbn(book.getIsbn());
+        exsistingBook.setMemo(book.getMemo());
+        exsistingBook.setStatus(book.getStatus());
+        exsistingBook.setUpdatedAt(book.getUpdatedAt());
+        return bookRepository.save(exsistingBook);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable long id){
+        Book exsistingBook = bookRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"該当のIDは存在していません。"));
+        bookRepository.deleteById(exsistingBook.getId());
     }
 }
