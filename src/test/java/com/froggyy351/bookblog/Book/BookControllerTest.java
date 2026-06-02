8b9db0de1511a66e08a16651238bd66612fb5c43
sakoichi <kouichi19980817@gmail.com>
2026-06-02 23:57:09 +0900

    feat: BookControllerTestの骨組みを新規作成

---
+++ src/test/java/com/froggyy351/bookblog/Book/BookControllerTest.java
@@ -0,0 +1,20 @@
+package com.froggyy351.bookblog.Book;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(BookController.class)
+public class BookControllerTest {
+
+    @MockitoBean
+    BookService bookService;
+
+    @Autowired
+    private MockMvc mockMvc;
+}
