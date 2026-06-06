50dbec729d3b4d9d5d663386f1611279ad404c08
sakoichi <kouichi19980817@gmail.com>
2026-06-06 18:54:01 +0900

    feat: C,U,DのエンドポイントUT追加

--- src/test/java/com/froggyy351/bookblog/Book/BookControllerTest.java
+++ src/test/java/com/froggyy351/bookblog/Book/BookControllerTest.java
@@ -3,14 +3,17 @@ package com.froggyy351.bookblog.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
-import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -60,12 +63,67 @@ public class BookControllerTest {
      * 異常系：selectBooks（１件）
      */
     @Test
-    public void selectBooks_withNoExistingId_returns200() throws Exception{
+    public void selectBooks_withNoExistingId_returns404() throws Exception{
         //Arrange
         when(bookService.findById(99L)).thenThrow(BookNotFoundException.class);
 
         //Act+Assert
         mockMvc.perform(get("/api/books/99"))
                 .andExpect(status().isNotFound());
+    }
+
+    /**
+     * 正常系：updateBook
+     */
+    @Test
+    public void updateBook_withId_returns200() throws Exception{
+        //Arrange
+        Book book = new Book();
+        book.setId(1L);
+        book.setTitle("テスト本");
+        when(bookService.update(eq(1L), any(Book.class))).thenReturn(book);
+
+        //Act+Assert
+        mockMvc.perform(put("/api/books/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"title\":\"テスト本update\", " +
+                        "\"author\":\"テストさん\", " +
+                        "\"isbn\":\"978-4-06-139801-6\", " +
+                        "\"status\":\"1\"}"))
+                .andExpect(status().isOk());
+    }
+
+    /**
+     * 正常系：createBook
+     */
+    @Test
+    public void createBook_withBook_returns200() throws Exception{
+        //Arrange
+        Book book = new Book();
+        book.setId(1L);
+        book.setTitle("テスト本");
+        when(bookService.create(any(Book.class))).thenReturn(book);
+
+        //Act+Assert
+        mockMvc.perform(post("/api/books")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"title\":\"テスト本\", " +
+                                "\"author\":\"テストさん\", " +
+                                "\"isbn\":\"978-4-06-139801-6\", " +
+                                "\"status\":\"1\"}"))
+                .andExpect(status().isOk());
+    }
+
+    /**
+     * 正常系：deleteBook
+     */
+    @Test
+    public void deleteBook_withId_returns200() throws Exception{
+        //Arrange
+        doNothing().when(bookService).delete(1L);
+
+        //Act+Assert
+        mockMvc.perform(delete("/api/books/1"))
+                .andExpect(status().isOk());
     }
 }
