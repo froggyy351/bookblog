7fad5edd31df4adb2289cfbaba9a5232a075c832
sakoichi <kouichi19980817@gmail.com>
2026-06-07 22:56:51 +0900

    feat: Userエンティティ、リポジトリを追加

---
+++ src/main/java/com/froggyy351/bookblog/User/User.java
@@ -0,0 +1,69 @@
+package com.froggyy351.bookblog.User;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name" ,nullable = false)
+    @NotBlank
+    @Size(max = 200)
+    private String userName;
+
+    @Column(name = "password" ,nullable = false)
+    @NotBlank
+    @Size(max = 200)
+    private String password;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public long getId() {
+        return id;
+    }
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
